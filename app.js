d11c875d7509a7efa45516c351e98b0603d3dd87
KrzysztofHum <k.humienny1996@protonmail.com>
2021-02-22 10:25:25 +0100

    TodoS localStorage

--- app.js
+++ app.js
@@ -137,6 +137,4 @@ function removeLocalTodos(todo){
     const todoIndex = todo.children[0].innerText
     todos.splice(todos.indexOf(todoIndex), 1)
     localStorage.setItem('todos', JSON.stringify(todos))
-
-    console.log(todo)
 }
